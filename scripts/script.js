8ed7ab7435ea5fb70c3e1d0eacc75113a09e7cf2
Praneet Bhoj <praneet.bhoj@gmail.com>
2021-05-13 22:48:48 -0700

    test without service worker

--- scripts/script.js
+++ scripts/script.js
@@ -8,17 +8,17 @@ let heading = document.querySelector('header > h1');
 
 // Make sure you register your service worker here too
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/sw.js').then(function (registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function (err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function () {
+//     navigator.serviceWorker.register('/sw.js').then(function (registration) {
+//       // Registration was successful
+//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//     }, function (err) {
+//       // registration failed :(
+//       console.log('ServiceWorker registration failed: ', err);
+//     });
+//   });
+// }
 
 document.addEventListener('DOMContentLoaded', () => {
   
--- sw.js
+++ sw.js
@@ -1,59 +1,59 @@
-// sw.js - Service Worker
+// // sw.js - Service Worker
 
-// You will need 3 event listeners:
-//   - One for installation
-//   - One for activation ( check out MDN's clients.claim() for this step )
-//   - One for fetch requests
+// // You will need 3 event listeners:
+// //   - One for installation
+// //   - One for activation ( check out MDN's clients.claim() for this step )
+// //   - One for fetch requests
 
-const CACHE_NAME = 'journal_entries_cache';
-let urlsToCache = [
-    '/',
-    '/components/entry-page.js',
-    '/components/journal-entry.js',
-    '/scripts/router.js',
-    '/scripts/script.js'
-];
+// const CACHE_NAME = 'journal_entries_cache';
+// let urlsToCache = [
+//     '/',
+//     '/components/entry-page.js',
+//     '/components/journal-entry.js',
+//     '/scripts/router.js',
+//     '/scripts/script.js'
+// ];
 
-self.addEventListener('install', function (event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function (cache) {
-          console.log('Opened cache');
-          return cache.addAll(urlsToCache);
-      })
-  );
-});
+// self.addEventListener('install', function (event) {
+//   // Perform install steps
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(function (cache) {
+//           console.log('Opened cache');
+//           return cache.addAll(urlsToCache);
+//       })
+//   );
+// });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         // Cache hit - return response
+//         if (response) {
+//           return response;
+//         }
 
-        return fetch(event.request).then(
-          function(response) {
-            // Check if we received a valid response
-            if(response && response.status == 200 && response.type == 'basic') {
+//         return fetch(event.request).then(
+//           function(response) {
+//             // Check if we received a valid response
+//             if(response && response.status == 200 && response.type == 'basic') {
               
-              // clone response for both browser and cache
-              var responseToCache = response.clone();
+//               // clone response for both browser and cache
+//               var responseToCache = response.clone();
 
-              caches.open(CACHE_NAME)
-                .then(function(cache) {
-                  cache.put(event.request, responseToCache);
-                });
-            }
+//               caches.open(CACHE_NAME)
+//                 .then(function(cache) {
+//                   cache.put(event.request, responseToCache);
+//                 });
+//             }
               
-            return response;
-          });
-      })
-  );
-});
+//             return response;
+//           });
+//       })
+//   );
+// });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(clients.claim());
-});
+// self.addEventListener('activate', event => {
+//   event.waitUntil(clients.claim());
+// });
